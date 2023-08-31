763d517620d91718a69a5b9ded6fe46925606609
DESKTOP-GU157D3\C <normallife3512@gmail.com>
2023-08-31 17:59:55 +0900

    test push 001

--- bookstok-app/src/App.js
+++ bookstok-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React test push 001
         </a>
       </header>
     </div>
