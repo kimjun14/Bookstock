f6110b12ba1668625c453274f87c1acfe25a9db9
DESKTOP-0RS2OMA\kimjun <kimjun14@naver.com>
2023-09-05 09:33:47 +0900

    https://github.com/kimjun14/Bookstock

---
+++ bookstok-api-server/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mysql: {
+    host: '3.36.124.52',
+    port: '3306',
+    // host: '127.0.0.1',
+    // port: '3306',
+    database: 'bookstock',
+    user: 'Bookstock12$$',
+    password: '1234'
+  }
+};
