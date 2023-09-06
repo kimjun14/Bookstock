3ef9b5fb47557e5ea0f5778e23d84ea372374799
DESKTOP-0RS2OMA\kimjun <kimjun14@naver.com>
2023-09-06 16:15:04 +0900

    홈 라우터 추가/김명준/

--- bookstok-app/src/App.js
+++ bookstok-app/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import SignIn from "./pages/signIn";
 import SearchResult from "./pages/searchResult";
 import Trading from "./pages/Trading";
+import MainPage from "./pages/mainPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/searchResult" element={<SearchResult />} />
-          <Route path="/trading" element={<Trading />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/searchResult" element={<SearchResult />} />
+        <Route path="/trading" element={<Trading />} />
       </Routes>
     </Router>
   );
