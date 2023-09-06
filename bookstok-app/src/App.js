46f561ebcda50bb66f810cfe7ec3b36c53ce2f3d
SANGMIN <kwk680037@gmail.com>
2023-09-06 15:27:05 +0900

    상품상세Page, logo,bookImg / 유상민 / App.js 에 상품상세Page 라우터 추가/

--- bookstok-app/src/App.js
+++ bookstok-app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import SignIn from "./pages/signIn";
 import SearchResult from "./pages/searchResult";
+import Trading from "./pages/Trading";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/searchResult" element={<SearchResult />} />
+          <Route path="/Trading" element={<Trading />} />
       </Routes>
     </Router>
   );
