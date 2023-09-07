69f2f4d1ca35de0b00dfcf3242a7ef44132ab8fd
SANGMIN <kwk680037@gmail.com>
2023-09-07 16:48:27 +0900

    Header.js제작 / 유상민

--- bookstok-app/src/components/Layout/header.js
+++ bookstok-app/src/components/Layout/header.js
@@ -1,80 +1,64 @@
-const Header = function(){
-    <header>
-    {/* 메인 상단 로고 */}
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <div id="logo">
-                <a href="/">
-                    <img
-                        className="logoImg"
-                        src="./bookStockLogo 복사본.jpg"
-                        alt="로고"
-                        style={{
-                            width: '200px',
-                            marginLeft: '30%',
-                        }}
-                    />
+import React from "react";
+
+const Header = function () {
+    return (
+        <>
+            <nav className="navbar bg-body-tertiary">
+        {/* <!-- logo --> */}
+        <div className="container">
+        <a className="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24" />
+        </a>
+        {/* <!-- searchbar --> */}
+        <form className="d-flex ms-auto" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        {/* <!-- headerNav --> */}
+        <ul className="nav justify-content-end">
+            <li className="nav-item">
+            <a className="nav-link active text-secondary-emphasis" aria-current="page" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
+                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
+                    <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+                </svg>
+            </a>
+            </li>
+            <li className="nav-item">
+
+            <a className="nav-link text-secondary-emphasis" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-door-open-fill" viewBox="0 0 16 16">
+                    <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                </svg>
+            </a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link text-secondary-emphasis" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
+                    <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                </svg>
+            </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-secondary-emphasis" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
                 </a>
+            </li>
+            <div className="collapse" id="navbarToggleExternalContent" data-bs-theme="bg-white">
+                <div className="bg-white p-1">
+                  <h5 className="text-body-emphasis h6">Collapsed content</h5>
+                  <span className="text-body-tertiary">Toggleable via the navbar brand.</span>
+                </div>
             </div>
-            <form
-                className="d-flex"
-                role="search"
-                style={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                }}
-            >
-                <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="도서를 검색하세요."
-                    aria-label="Search"
-                    style={{ width: '500px' }}
-                />
-                <button type="submit" style={{ border: 'none', background: 'none', padding: '0' }}>
-                    <i className="fa-solid fa-magnifying-glass" style={{ color: '#000000', marginLeft: '10px' }}></i>
-                </button>
-            </form>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ position: 'absolute', top: '50%', left: '90%' }}>
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li id="category">
-                        <button style={{ border: 'none', background: 'none', padding: '0', margin: '0 15px' }}>
-                            <i className="fa-solid fa-bars" style={{ color: '#000000' }}></i>
-                        </button>
-                    </li>
-                    <li id="Home">
-                        <button style={{ border: 'none', background: 'none', padding: '0', margin: '0 15px' }}>
-                            <i className="fa-solid fa-house" style={{ color: '#000000' }}></i>
-                        </button>
-                    </li>
-                    <li id="Profile">
-                        <button style={{ border: 'none', background: 'none', padding: '0', margin: '0 15px' }}>
-                            <i className="fa-solid fa-user" style={{ color: '#000000' }}></i>
-                        </button>
-                    </li>
-                    <li id="Login">
-                        <button style={{ border: 'none', background: 'none', padding: '0' }}>
-                            <i className="fa-solid fa-arrow-right-to-bracket" style={{ color: '#000000' }}></i>
-                        </button>
-                    </li>
-                </ul>
-            </div>
+        </ul>
         </div>
     </nav>
-</header>
+        </>
+    )
 }
-
 export default Header;
