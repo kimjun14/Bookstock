5f9e5a3aefa25b4a5043fca1bbfd17a73a5a85e9
DESKTOP-GU157D3\C <normallife3512@gmail.com>
2023-09-11 13:50:10 +0900

    API 서버 / 회원 기능 마무리 / 차동혁

--- bookstok-api-server/routes/index.js
+++ bookstok-api-server/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const userRouter = require('./users');
-// const AuctionRouter = require('./aucboard'); 경매기능
+const AuctionRouter = require('./aucboard'); // 경매기능
 
 router.use('/users', userRouter);
-// router.use('/aucboards', aucRouter);
+router.use('/auctions', AuctionRouter);
 
 module.exports = router;
