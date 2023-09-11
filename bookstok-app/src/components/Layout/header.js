6e1ecda2a3d9c0d202daf8664560b6b866f2aa43
SANGMIN <kwk680037@gmail.com>
2023-09-11 11:41:46 +0900

    header.js 충돌 후 다시 커밋 / 유상민

--- bookstok-app/src/components/Layout/header.js
+++ bookstok-app/src/components/Layout/header.js
@@ -5,54 +5,11 @@ import logo2 from "../../img/logo2.jpg";
 const Header = function () {
     return (
         <>
-<<<<<<< HEAD
-        <nav className="navbar bg-white">
-            {/* <!-- logo --> */}
-            <div className="container mb-3">
-            <a className="navbar-brand" href="/">
-                <img src={logo2} alt="logo" width="150" height="130" />
-            </a>
-            {/* <!-- searchbar --> */}
-            <form className="d-flex ms-auto" role="search">
-                <input className="form-control me-2" type="search" placeholder="search" />
-                <button className="btn btn-outline-primary" type="submit">search</button>
-            </form>
-            {/* <!-- headerNav --> */}
-            <ul className="nav justify-content-end">
-                <li className="nav-item">
-                <a className="nav-link active text-secondary-emphasis" href="/"> {/* 메인 화면 */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
-                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
-                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
-                    </svg>
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link text-secondary-emphasis" href="/signin"> {/* 로그인 */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-door-open-fill" viewBox="0 0 16 16">
-                        <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
-                    </svg>
-                </a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link text-secondary-emphasis" href="/register"> {/* 회원 가입 */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                    </svg>
-                </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-secondary-emphasis" href="/"> {/* 마이 페이지 */}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                        </svg>
-=======
             <nav className="navbar bg-white">
                 {/* <!-- logo --> */}
                 <div className="container mb-3">
                     <a className="navbar-brand" href="/">
                         <img src={logo2} alt="logo" width="150" height="130" />
->>>>>>> 17ff25bcec2b7ad5d4d77ec83b2e30c3b4f2570b
                     </a>
                     {/* <!-- searchbar --> */}
                     <form className="d-flex ms-auto" role="search">
