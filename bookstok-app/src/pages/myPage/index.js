ba165dae15fb3d29c5c1206097872ea0ad53b021
최한슬 <hanseulo@naver.com>
2023-09-12 10:35:54 +0900

    my,regi,search/반응형수정/최한슬

--- bookstok-app/src/pages/myPage/index.js
+++ bookstok-app/src/pages/myPage/index.js
@@ -1,120 +1,121 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 
 const MyPage = () => {
-    return(
+    return (
         <div className="container">
-        <div className="row">
-            <div className="col-md-2" style={{ marginLeft: "4rem" }}>
-                <p>마이북스탁</p>
-                <Link to="/mypage" className="text-decoration-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-house-door-fill" viewBox="0 0 16 16">
-                        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+            <div className="row">
+                <div className="col-md-2" style={{ marginLeft: "4rem" }}>
+                    <p>마이북스탁</p>
+                    <Link to="/mypage" className="text-decoration-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-house-door-fill" viewBox="0 0 16 16">
+                            <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
+                        </svg>
+                        <span className="text-black"> 마이북스탁 홈</span>
+                    </Link>
+                    <hr />
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-book-fill" viewBox="0 0 16 16">
+                        <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                     </svg>
-                    <span className="text-black"> 마이북스탁 홈</span>
-                </Link>
-                <hr />
+                    <span> 책</span>
+                    <br />
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-book-fill" viewBox="0 0 16 16">
-                    <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-                </svg>
-                <span> 책</span>
-                <br />
+                    <Link to="/" className="ms-4 text-decoration-none text-black">선호상품</Link><br />
+                    <Link to="/" className="ms-4 text-decoration-none text-black">최근 조회한 상품</Link><br />
+                    <hr />
 
-                <Link to="/" className="ms-4 text-decoration-none text-black">선호상품</Link><br />
-                <Link to="/" className="ms-4 text-decoration-none text-black">최근 조회한 상품</Link><br />
-                <hr />
-
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-person-fill" viewBox="0 0 16 16">
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                </svg>
-                <span> 개인</span>
-                <br />
-
-                <Link className="ms-4 text-decoration-none text-black" to="/">내 정보 관리</Link><br />
-                <Link className="ms-4 text-decoration-none text-black" to="/">1:1 문의</Link><br />
-                <Link className="ms-4 text-decoration-none text-black" to="/">기기 관리</Link><br />
-                <Link className="ms-4 text-decoration-none text-black" to="/">리뷰 관리</Link><br />
-            </div>
-
-            <div className="col-md-9" style={{marginTop: "10px;"}}>
-                <div className="col-md-12 p-4 rounded d-flex flex-row" style={{border: "1px solid #bcbdbe"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" fill="#bcbdbe" className="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                     </svg>
+                    <span> 개인</span>
+                    <br />
 
-                    <div className="ms-4 mt-2" style={{border: "0px solid blue"}}>
-                        <span>
-                            <b className="fs-5">userId</b>
-                        </span><br />
-                        <span className="text-black-50">
-                            email
-                        </span><br />
-                        <button className="mt-1 btn btn-secondary btn-sm">프로필 관리</button>
-                    </div>
-
-                    <div style={{borderRight: "1px solid #bcbdbe", lineHeight: "5.9rem", marginLeft: "27rem"}}>
-                        <p className="mx-5">
-                            <b>일반회원</b>
-                        </p>
-                    </div>
-
-                    <div style={{lineHeight: "5.9rem", marginLeft: "3rem"}}>
-                        <p>
-                            <b>예치금</b>
-                        </p>
-                    </div>
+                    <Link className="ms-4 text-decoration-none text-black" to="/">내 정보 관리</Link><br />
+                    <Link className="ms-4 text-decoration-none text-black" to="/">1:1 문의</Link><br />
+                    <Link className="ms-4 text-decoration-none text-black" to="/">기기 관리</Link><br />
+                    <Link className="ms-4 text-decoration-none text-black" to="/">리뷰 관리</Link><br />
                 </div>
 
-                <div className="fs-4 mt-5 mb-4 ms-2 fw-bold">경매 진행 내역</div>
+                <div className="col-md-9" style={{ marginTop: "10px" }}>
+                    <div className="col-md-12 p-4 rounded d-flex flex-row" style={{ border: "1px solid #bcbdbe" }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" fill="#bcbdbe" className="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
 
-                <div className="border rounded">
-                    <div className="d-flex flex-row mb-3 justify-content-around">
-                        <div className="p-2 ms-4">
-                            <Link to="#" className="text-decoration-none">
-                                <p className="fs-5 mt-4 text-black">입찰중</p>
-                                <p className="text-center text-black">0</p>
-                            </Link>
+                        <div className="ms-4 mt-2" style={{ border: "0px solid blue" }}>
+                            <span>
+                                <b className="fs-5">userId</b>
+                            </span><br />
+                            <span className="text-black-50">
+                                email
+                            </span><br />
+                            <button className="mt-1 btn btn-secondary btn-sm">프로필 관리</button>
                         </div>
 
-                        <div className="p-2 ms-4">
-                            <Link to="#" className="text-decoration-none">
-                                <p className="fs-5 mt-4 text-black">판매완료</p>
-                                <p className="text-center text-black">0</p>
-                            </Link>
+                        <div style={{ borderRight: "1px solid #bcbdbe", lineHeight: "5.9rem", marginLeft: "27rem" }}>
+                            <p className="mx-5">
+                                <b>일반회원</b>
+                            </p>
                         </div>
 
-                        <div className="p-2 ms-4">
-                            <Link to="#" className="text-decoration-none">
-                                <p className="fs-5 mt-4 text-black">역경매등록</p>
-                                <p className="text-center text-black">0</p>
-                            </Link>
+                        <div style={{ lineHeight: "5.9rem", marginLeft: "3rem" }}>
+                            <p>
+                                <b>예치금</b>
+                            </p>
                         </div>
-
-                        <div className="p-2 ms-4">
-                            <Link to="#" className="text-decoration-none">
-                                <p className="fs-5 mt-4 text-black">구매중</p>
-                                <p className="text-center text-black">0</p>
-                            </Link>
-                        </div>
-
-                        <div className="p-2 ms-4">
-                            <Link to="#" className="text-decoration-none">
-                                <p className="fs-5 mt-4 text-black">구매완료</p>
-                                <p className="text-center text-black">0</p>
-                            </Link>
-                        </div>    
                     </div>
-                </div>
 
-                <div className="mt-5 ">
-                    <div>
-                        <p className="text-center">거래 내역이 없습니다.</p>
+                    <div className="fs-4 mt-5 mb-4 ms-2 fw-bold">경매 진행 내역</div>
+
+                    <div className="border rounded">
+                        <div className="d-flex flex-row mb-3 justify-content-around">
+                            <div className="p-2 ms-4">
+                                <Link to="#" className="text-decoration-none">
+                                    <p className="fs-5 mt-4 text-black">입찰중</p>
+                                    <p className="text-center text-black">0</p>
+                                </Link>
+                            </div>
+
+                            <div className="p-2 ms-4">
+                                <Link to="#" className="text-decoration-none">
+                                    <p className="fs-5 mt-4 text-black">판매완료</p>
+                                    <p className="text-center text-black">0</p>
+                                </Link>
+                            </div>
+
+                            <div className="p-2 ms-4">
+                                <Link to="#" className="text-decoration-none">
+                                    <p className="fs-5 mt-4 text-black">역경매등록</p>
+                                    <p className="text-center text-black">0</p>
+                                </Link>
+                            </div>
+
+                            <div className="p-2 ms-4">
+                                <Link to="#" className="text-decoration-none">
+                                    <p className="fs-5 mt-4 text-black">구매중</p>
+                                    <p className="text-center text-black">0</p>
+                                </Link>
+                            </div>
+
+                            <div className="p-2 ms-4">
+                                <Link to="#" className="text-decoration-none">
+                                    <p className="fs-5 mt-4 text-black">구매완료</p>
+                                    <p className="text-center text-black">0</p>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-5 ">
+                        <div>
+                            <p className="text-center">거래 내역이 없습니다.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
--- bookstok-app/src/pages/searchResult/index.js
+++ bookstok-app/src/pages/searchResult/index.js
@@ -1,189 +1,80 @@
-// 검색 페이지 제작
-
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SearchResult() {
-return (
-<>
-{/* 부트스트랩 css 라이브러리 이용 */}
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-  crossorigin="anonymous"
-/>
-{/* Body , 검색 결과 제공*/ }
-<div className='container bg-light'>
-  {/* 검색 창 표시 row */ }
-  {/* 검색 버튼 누르면 ?title(key)=검색내용(value)를 get으로 던짐 */}
-  <div className='row'>
-    <form action="/" method="get">
-      <div class="input-group p-3 d-flex justify-content-center">
-        <input className='w-75 me-2' type="text" placeholder="책 이름 검색" name="title" />
-        <span class="input-group-append">
-          <button class="btn btn-primary" type="submit">검색</button>
-        </span>
+  return (
+    <>
+      {/* 부트스트랩 css 라이브러리 이용 */}
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossOrigin="anonymous"
+      />
+      {/* Body, 검색 결과 제공 */}
+      <div className="container bg-light">
+        {/* 검색 창 표시 row */}
+        {/* 검색 버튼 누르면 ?title(key)=검색내용(value)를 get으로 던짐 */}
+        <div className="row">
+          <form action="/" method="get">
+            <div className="input-group p-3 d-flex justify-content-center">
+              <input
+                className="w-75 me-2"
+                type="text"
+                placeholder="책 이름 검색"
+                name="title"
+              />
+              <span className="input-group-append">
+                <button className="btn btn-primary" type="submit">
+                  검색
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+        {/* 검색 내용 표시 row */}
+        {[1, 2, 3, 4, 5].map((item) => (
+          <div
+            key={item}
+            className="row p-2 d-flex align-items-center"
+          >
+            <div className="col-md-3 col-sm-6">
+              <Link to="/trading">
+                <img
+                  className="img-fluid"
+                  alt="searchImg"
+                  src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg"
+                />
+              </Link>
+            </div>
+            <div className="col-md-9 col-sm-6">
+              <div className="d-flex align-items-start">
+                <span className="col-2">제목</span>
+                <span className="col-10">: 타입스크립트, 리액트, Next.js...</span>
+              </div>
+              <div className="d-flex align-items-start">
+                <span className="col-2">작가</span>
+                <span className="col-10">: 테지마 타쿠야...</span>
+              </div>
+              <div className="d-flex align-items-start">
+                <span className="col-2">출판사</span>
+                <span className="col-10">: 위키북스</span>
+              </div>
+              <div className="d-flex align-items-start">
+                <span className="col-2">출판일</span>
+                <span className="col-10">: 20230530</span>
+              </div>
+              <div className="d-flex align-items-start">
+                <span className="col-2">ISBN</span>
+                <span className="col-10">: 9791158394332</span>
+              </div>
+              <div>TypeScript/React/Next.js로 실전적인...</div>
+            </div>
+          </div>
+        ))}
       </div>
-    </form>
-  </div>
-  {/* 검색 내용 표시 row 1 */ }
-  <div className='row p-2 d-flex align-items-center'>
-    <div className='col-3'>
-      <Link to ='/trading'>
-        <img className='img-fluid' alt='searchImg' src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg" />
-      </Link>
-    </div>
-    <div className='col-9'>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>제목</span>
-        <span className='col-10'>: 타입스크립트, 리액트, Next.js...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>작가</span>
-        <span className='col-10'>: 테지마 타쿠야...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판사</span>
-        <span className='col-10'>: 위키북스</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판일</span>
-        <span className='col-10'>: 20230530</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>ISBN</span>
-        <span className='col-10'>: 9791158394332</span>
-      </div>
-      <div>TypeScript/React/Next.js로 실전적인...</div>
-    </div>
-  </div>
-  {/* 검색 내용 표시 row 2 */ }
-  <div className='row p-2 d-flex align-items-center'>
-    <div className='col-3'>
-      <Link to ='/trading'>
-        <img className='img-fluid' alt='searchImg' src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg" />
-      </Link>
-    </div>
-    <div className='col-9'>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>제목</span>
-        <span className='col-10'>: 타입스크립트, 리액트, Next.js...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>작가</span>
-        <span className='col-10'>: 테지마 타쿠야...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판사</span>
-        <span className='col-10'>: 위키북스</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판일</span>
-        <span className='col-10'>: 20230530</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>ISBN</span>
-        <span className='col-10'>: 9791158394332</span>
-      </div>
-      <div>TypeScript/React/Next.js로 실전적인...</div>
-    </div>
-  </div>
-  {/* 검색 내용 표시 row 3 */ }
-  <div className='row p-2 d-flex align-items-center'>
-    <div className='col-3'>
-      <Link to ='/trading'>
-          <img className='img-fluid' alt='searchImg' src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg" />
-      </Link>
-    </div>
-    <div className='col-9'>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>제목</span>
-        <span className='col-10'>: 타입스크립트, 리액트, Next.js...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>작가</span>
-        <span className='col-10'>: 테지마 타쿠야...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판사</span>
-        <span className='col-10'>: 위키북스</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판일</span>
-        <span className='col-10'>: 20230530</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>ISBN</span>
-        <span className='col-10'>: 9791158394332</span>
-      </div>
-      <div>TypeScript/React/Next.js로 실전적인...</div>
-    </div>
-  </div>
-  {/* 검색 내용 표시 row 4 */ }
-  <div className='row p-2 d-flex align-items-center'>
-    <div className='col-3'>
-      <Link to ='/trading'>
-          <img className='img-fluid' alt='searchImg' src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg" />
-      </Link>
-    </div>
-    <div className='col-9'>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>제목</span>
-        <span className='col-10'>: 타입스크립트, 리액트, Next.js...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>작가</span>
-        <span className='col-10'>: 테지마 타쿠야...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판사</span>
-        <span className='col-10'>: 위키북스</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판일</span>
-        <span className='col-10'>: 20230530</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>ISBN</span>
-        <span className='col-10'>: 9791158394332</span>
-      </div>
-      <div>TypeScript/React/Next.js로 실전적인...</div>
-    </div>
-  </div>
-  {/* 검색 내용 표시 row 5 */ }
-  <div className='row p-2 d-flex align-items-center'>
-    <div className='col-3'>
-      <Link to ='/trading'>
-        <img className='img-fluid' alt='searchImg' src="https://shopping-phinf.pstatic.net/main_4019202/40192022618.20230620100239.jpg" />
-      </Link>
-    </div>
-    <div className='col-9'>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>제목</span>
-        <span className='col-10'>: 타입스크립트, 리액트, Next.js...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>작가</span>
-        <span className='col-10'>: 테지마 타쿠야...</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판사</span>
-        <span className='col-10'>: 위키북스</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>출판일</span>
-        <span className='col-10'>: 20230530</span>
-      </div>
-      <div className='d-flex align-items-start'>
-        <span className='col-2'>ISBN</span>
-        <span className='col-10'>: 9791158394332</span>
-      </div>
-      <div>TypeScript/React/Next.js로 실전적인...</div>
-    </div>
-  </div>
-</div>
-</>
-);
-};
+    </>
+  );
+}
 
 export default SearchResult;
