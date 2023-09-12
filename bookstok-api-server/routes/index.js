22de58e79c0b61910d28507fae30289656a04c96
DESKTOP-GU157D3\C <normallife3512@gmail.com>
2023-09-12 17:43:53 +0900

    api 서버 / 검색 기능을 위한 API 중계 기능 구현 / 차동혁

--- bookstok-api-server/routes/index.js
+++ bookstok-api-server/routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 const userRouter = require('./users');
 const AuctionRouter = require('./aucboard'); // 경매기능
+const searchRouter = require('./search');
 
 router.use('/users', userRouter);
 router.use('/auctions', AuctionRouter);
+router.use('/search',searchRouter);
 
 module.exports = router;
---
+++ bookstok-api-server/routes/search.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+
+// 검색어 쿼리 받은걸 외부 API 서버로 던짐
+router.post('/', async (req, res, next) => {
+  const { query } = req.body;
+
+  const apiId = "H38Bmj5zQN_R3r2xt1w0";
+  const apiKey = "qcEHvvlOdU";
+  try {
+    // 외부 API에 요청
+    const response = await axios.get('https://openapi.naver.com/v1/search/book.json',{
+      params: 
+      { query: query,
+        start: 1,
+        display:10  
+      },
+      headers: {
+        "X-Naver-Client-Id" : apiId,
+        "X-Naver-Client-Secret" : apiKey
+      }
+    })
+    console.log(response.data);
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
