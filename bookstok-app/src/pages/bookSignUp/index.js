e1833e44a870777af7a29682a82b87ac411e18d4
SANGMIN <kwk680037@gmail.com>
2023-09-13 11:06:31 +0900

    도서 등록 페이지 도서검색 버튼 누를시 팝업 / App.css 팝업 추가 / 유상민

--- bookstok-app/src/pages/bookSignUp/index.js
+++ bookstok-app/src/pages/bookSignUp/index.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 
 const BookSignUp = () => {
+    const [showPopup, setShowPopup] = useState(false); // New state for popup visibility
+    const [popupContent, setPopupContent] = useState(""); // New state for popup content
     const [product, setProduct] = useState({
         name: '',
         description: '',
@@ -29,17 +31,39 @@ const BookSignUp = () => {
             category: '',
         });
     };
-    const handleBookSearch = () => {
-        // 도서 검색 로직 구현
-        console.log('Searching for books...');
+
+    const handlePopUp = () => {
+        console.log("팝업구현");
+        // Set the popup content here (e.g., a form, additional information, etc.)
+        setPopupContent(
+            <div className="popup">
+                <div className="popup-content">
+                    <h2>팝업 제목</h2>
+                    <p>팝업 내용</p>
+                    <button onClick={closePopup}>닫기</button>
+                </div>
+            </div>
+        );
+
+        // Show the popup by setting showPopup to true
+        setShowPopup(true);
     };
+
+    const closePopup = () => {
+        // Close the popup by setting showPopup to false
+        setShowPopup(false);
+    
+        // Optionally, you can clear the popup content
+        setPopupContent("");
+    };
+
 
     return (
         <>
             <div className="container-fluid mt-5">
                 <div className="row justify-content-around">
                     <div className="col-md-auto">
-                        <img src="http://placeholder.com/450x420" />
+                        <img src="http://placeholder.com/450x420" alt="bookimg" />
                     </div>
                     <div className="col-md-6">
                         <h2 className="mb-3">도서 역경매 등록</h2>
@@ -53,7 +77,6 @@ const BookSignUp = () => {
                                     name="name"
                                     value={product.name}
                                     onChange={handleChange}
-                                    required
                                 />
                             </div>
                             <div className="form-group">
@@ -64,7 +87,6 @@ const BookSignUp = () => {
                                     name="description"
                                     value={product.description}
                                     onChange={handleChange}
-                                    required
                                 ></textarea>
                             </div>
                             <div className="form-group">
@@ -76,7 +98,6 @@ const BookSignUp = () => {
                                     name="price"
                                     value={product.price}
                                     onChange={handleChange}
-                                    required
                                 />
                             </div>
                             <div className="form-group">
@@ -94,7 +115,7 @@ const BookSignUp = () => {
                                 <div className="col-md-6 mt-5">
                                     <div>
                                         <button
-                                            onClick={handleBookSearch}
+                                            onClick={handlePopUp}
                                             className="btn btn-info btn-lg text-white"
                                         >
                                             도서 검색
@@ -102,10 +123,11 @@ const BookSignUp = () => {
                                     </div>
                                 </div>
                             </div>
+                            {showPopup && popupContent}
                         </form>
                     </div>
                 </div>
-                
+
                 <div className="d-grid gap-2 col-11 mx-auto mt-5">
                     <button type="submit" className="btn btn-primary btn-lg mt-5">
                         역경매 등록
