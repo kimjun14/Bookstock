ee29b9ebd21b4450fc88969ee5cf5fb036f58b42
DESKTOP-0RS2OMA\kimjun <kimjun14@naver.com>
2023-09-14 15:01:38 +0900

    class, fill-rule 수정/header.js/김명준

--- bookstok-api-server/routes/users.js
+++ bookstok-api-server/routes/users.js
@@ -4,6 +4,7 @@ const crypto=require('crypto');
 
 
 const user = require('../models/user.model');
+const { clear } = require('console');
 
 // 사용자 목록 조회 [get] ip:12345/api/users/
 router.get('/', async (req, res, next) => {
@@ -38,8 +39,10 @@ router.post('/signin', async (req, res, next) => {
     if (ok) {
       // 아이디와 비밀번호가 일치하는 경우 세션에 사용자 아이디를 저장
       req.session.userId = userId;
-
+      console.log("새션 생성", req.session);
       res.status(200).send({ message: 'SUCCESS' });
+      req.session = clear()
+      console.log("새션 삭제", req.session);
     } else {
       // 아이디 또는 비밀번호가 일치하지 않는 경우
       res.status(401).send({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
--- bookstok-app/src/App.js
+++ bookstok-app/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import './App.css';
 import SignIn from "./pages/signIn";
-import { AuthProvider } from './AuthContext';
 import SearchResult from "./pages/searchResult/newIndex";
 import Trading from "./pages/Trading";
 import MainPage from "./pages/mainPage";
@@ -11,22 +10,22 @@ import MyPage from "./pages/myPage";
 import BookSignUp from "./pages/bookSignUp";
 
 function App() {
+
+
   return (
-    <AuthProvider> {/* AuthProvider로 감싸서 로그인 상태 관리 */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/searchResult" element={<SearchResult />} />
-            <Route path="/trading" element={<Trading />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/booksignup" element={<BookSignUp />} />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/searchResult" element={<SearchResult />} />
+          <Route path="/trading" element={<Trading />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/booksignup" element={<BookSignUp />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
