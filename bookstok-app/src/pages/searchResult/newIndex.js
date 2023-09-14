291f6c624e7f8cad2f5bb5d7c09389338595d851
최한슬 <hanseulo@naver.com>
2023-09-14 11:46:15 +0900

    검색결과페이지/가격,관심버튼 수정/최한슬

--- bookstok-app/src/pages/searchResult/newIndex.js
+++ bookstok-app/src/pages/searchResult/newIndex.js
@@ -9,8 +9,9 @@ function BookSearchResult() {
       id: 1,
       title: '어린왕자',
       coverImage: 'http://placeholder.com/50x70',
-      price: '11,000',
-      userId: '가을이',
+      startPrice: '11,000',
+      updatePrice: '9,000',
+      userId: '닉네임',
       startDate: '2023-09-12',
       endDate: '2023-09-27',
       views: 10,
@@ -20,7 +21,7 @@ function BookSearchResult() {
 
   return (
     <div>
-      <h1 style={{textAlign:'center'}}>도서 검색 결과</h1>
+      <h1 style={{ textAlign: 'center' }}>도서 검색 결과</h1>
       {/* 정렬 버튼 */}
       <div class="btn-group" role="group" aria-label="Basic example">
         <button type="button" class="btn btn-primary">최신순</button>
@@ -29,8 +30,9 @@ function BookSearchResult() {
       </div>
       {/* 검색 결과 표 */}
       <table className="searchTable">
-        <thead style={{backgroundColor:'#f3f3f3' 
-}}>
+        <thead style={{
+          backgroundColor: '#f3f3f3'
+        }}>
           <tr>
             <th scope="col"></th>
             <th scope="col">표지</th>
@@ -48,14 +50,15 @@ function BookSearchResult() {
               <th scope="row">{book.id}</th>
               <td><img src={book.coverImage} alt="" /></td>
               <td><Link to={`/book/${book.id}`}>{book.title}</Link></td>
-              <td>{book.price}</td>
+              <td>시작가: {book.startPrice}원
+                <br />현재가: {book.updatePrice}원</td>
               <td><Link to={`/author/${book.author}`}>{book.userId}</Link></td>
               <td>{book.startDate} /<br />{book.endDate}</td>
               <td>{book.views}</td>
               <td>
 
                 {/* 관심등록 버튼 */}
-                <button>
+                <button className='interestBtn'>
                   <img
                     src="https://cheolsusee.com/img/es_img/btn_rows_scrap_add_05.png"
                     alt=""
