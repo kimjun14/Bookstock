7a181594c87d0f30f5a14add3b605ed2f3a6463d
DESKTOP-GU157D3\C <normallife3512@gmail.com>
2023-09-15 13:39:26 +0900

    API 서버, 거래페이지 / 경매 아이디 쿼리를 받으면 해당 아이디 경매정보 출력하도록 변경 / 차동혁

--- bookstok-api-server/routes/aucboard.js
+++ bookstok-api-server/routes/aucboard.js
@@ -3,10 +3,11 @@ var router = express.Router();
 
 const user = require('../models/aucboard.model');
 
-// 경매 목록 조회 [get] ip:12345/api/auctions/
-router.get('/', async (req, res, next) => {
+// 경매 목록 조회 [get] ip:12345/api/auctions/49
+router.get('/:id', async (req, res, next) => {
   try{
-    const list = await user.find();
+    const id = Number(req.params.id);
+    const list = await user.auctionSearch(id);
     res.json(list);
   }catch(err){
     next(err);
