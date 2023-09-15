09ea503d468a55c4f5b2e6c3283fc2de3c3f779e
DESKTOP-GU157D3\C <normallife3512@gmail.com>
2023-09-15 15:50:34 +0900

    API 서버, 경매 페이지 / 입찰 기능 구현 / 차동혁

--- bookstok-api-server/routes/aucboard.js
+++ bookstok-api-server/routes/aucboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const user = require('../models/aucboard.model');
 
-// 경매 목록 조회 [get] ip:12345/api/auctions/49
+// 경매 목록 조회 [get] ip:12345/api/auctions/경매번호
 router.get('/:id', async (req, res, next) => {
   try{
     const id = Number(req.params.id);
@@ -17,8 +17,19 @@ router.get('/:id', async (req, res, next) => {
 // 경매등록  [post] ip:12345/api/auctions/
 router.post('/', async (req, res, next) => {
   try{
-    const id = await user.create(req.body);
+    const id = await user.addAuction(req.body);
     res.json({ id });
+  }catch(err){
+    next(err);
+  }
+});
+
+// 입찰등록  [post] ip:12345/api/auctions/경매번호
+router.post('/:id', async (req, res, next) => {
+  try{
+    const id = Number(req.params.id);
+    const result = await user.addBid(req.body,id);
+    res.json({ result });
   }catch(err){
     next(err);
   }
