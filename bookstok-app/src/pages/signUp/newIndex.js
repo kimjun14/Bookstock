4c3f97206e9ce87c5b468207fdac8a40b5ddbddf
최한슬 <hanseulo@naver.com>
2023-09-15 13:33:55 +0900

    signup/전화번호추가,css수정/최한슬

--- bookstok-app/src/pages/signUp/newIndex.js
+++ bookstok-app/src/pages/signUp/newIndex.js
@@ -5,22 +5,19 @@ import './newIndex.css'
 function SignUp() {
     return (
         <div className="container-fluid px-2 px-md-4 py-5 mx-auto">
-            <div className="col-lg-7" style={{margin:'auto'}}>
+            <div className="col-lg-7" style={{ margin: 'auto' }}>
                 <div className="card2 card border-0 px-4 px-sm-5 py-5">
                     <small className="text-right mb-3">
                         <Link to="./../signIn/newSignin.js">
                             <u>북스탁 계정이 이미 있나요?</u>
                         </Link>
                     </small>
-                    <h3 className="mb-1">회원가입</h3>
-                    {/* <p className="mb-4 text-sm">Create your account and start learning with thousands of courses</p> */}
-                    <div className="row mt-3">
-                        <div className="col-md-6">
-                            <label className="mb-0">
-                                <h6 className="mb-0 text-sm">닉네임</h6>
-                            </label>
-                            <input type="text" name="fname" placeholder="RaLiBooks" />
-                        </div>
+                    <h3 className="mb-1" >회원가입</h3>
+                    <div className="row px-3" style={{marginTop:'1rem'}}>
+                        <label className="mb-0">
+                            <h6 className="mb-0 text-sm">닉네임</h6>
+                        </label>
+                        <input type="text" name="nick" placeholder="RaLiBooks" style={{width:'50%'}}/>
                     </div>
                     <div className="row px-3">
                         <label className="mb-0">
@@ -38,13 +35,19 @@ function SignUp() {
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">주소</h6>
                         </label>
-                        <input type="text" name="email" placeholder="주소를 입력하세요" />
+                        <input type="text" name="adress" placeholder="주소를 입력하세요" />
+                    </div>
+                    <div className="row px-3">
+                        <label className="mb-0">
+                            <h6 className="mb-0 text-sm">전화번호</h6>
+                        </label>
+                        <input type="text" name="phone" placeholder="-(하이픈)을 제외하고 입력하세요" />
                     </div>
                     <div className="row px-3">
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">계좌번호</h6>
                         </label>
-                        <input type="text" name="email" placeholder="-(하이픈)을 제외하고 입력하세요" />
+                        <input type="text" name="acount" placeholder="-(하이픈)을 제외하고 입력하세요" />
                     </div>
                     <div className="row px-3 mb-3">
                         <small className="text-muted">
@@ -56,21 +59,22 @@ function SignUp() {
                             <button className="btn btn-blue text-center mb-1 py-2" >계정 만들기</button>
                         </div>
                     </div>
-                    <div className="row px-3 mb-4">
+                    <div className="row px-3 mb-4 d-flex align-items-center">
                         <div className="line"></div>
                         <small className="text-muted or text-center">OR</small>
                         <div className="line"></div>
                     </div>
+
                     <div className="row text-center">
                         <div className="col-sm-6">
                             <p className="social-connect1">
-                            <button className="btn btn-blue text-center mb-1 py-2">카카오 계정으로 로그인하기</button>
+                                <button className="btn text-center mb-1 py-2">카카오 계정으로 로그인하기</button>
                             </p>
                         </div>
                         <div className="col-sm-6">
                             <p className="social-connect2">
                                 <span className="fa fa-google-plus"></span>
-                                <button className="btn btn-blue text-center mb-1 py-2">네이버 계정으로 로그인하기</button>
+                                <button className="btn text-center mb-1 py-2">네이버 계정으로 로그인하기</button>
                             </p>
                         </div>
                         {/* <div className="col-sm-6">
