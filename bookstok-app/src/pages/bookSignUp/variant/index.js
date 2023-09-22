0fadea42a0314f21797b420a9dcb7ff9b365a152
최한슬 <hanseulo@naver.com>
2023-09-22 17:07:22 +0900

    bookSignUp/역경매등록css수정중/최한슬

--- bookstok-app/src/pages/bookSignUp/variant/index.js
+++ bookstok-app/src/pages/bookSignUp/variant/index.js
@@ -2,37 +2,35 @@ import { useEffect, useState } from "react";
 import BookResearch from './bookInfo';
 import axios from "axios";
 import { useNavigate } from "react-router";
+import './index.css'
 
-// axios 통신에 기본 url을 포함시키고 Credentials 옵션을 붙여서 쿠키전송 가능하게 함
 const axiosConnect = axios.create({
     baseURL: 'http://localhost:12345/api',
     withCredentials: true
 });
-  
 
 const BookSignUp = () => {
-    // 1. 상태 생성
     const [product, setProduct] = useState({
         auctionTitle: '',
         auctionContext: '',
         auctionPrice: '',
-        uId:'',   // 나중에 세션 아이디 받으면 API 서버 쿼리문과 같이 교체할 예정
-        bookImgSrc:'',
-        bookTitle:'',
-        bookAuthor:'',
-        bookPub:''
-
-        // bookPubDate 입력타입때문에 나중에 고쳐서 구현함
+        uId: '',
+        bookImgSrc: '',
+        bookTitle: '',
+        bookAuthor: '',
+        bookPub: '',
+        auctionEnd: '', // 초기값은 빈 문자열로 설정
     });
+    const [selectedDays, setSelectedDays] = useState(1); // 초깃값을 1로 설정
     const navigation = useNavigate();
 
     const infoCallback = (book) => {
-        setProduct(prevProduct => ({
+        setProduct((prevProduct) => ({
             ...prevProduct,
             bookTitle: book.TITLE,
             bookAuthor: book.AUTHOR,
             bookPub: book.PUBLISHER,
-            bookImgSrc:book.TITLE_URL
+            bookImgSrc: book.TITLE_URL
         }));
     }
 
@@ -40,96 +38,170 @@ const BookSignUp = () => {
         console.log(product);
     }, [product]);
 
-    // 2. 역경매 등록창 핸들러
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setProduct({
-            ...product,
+    const handleChange = (e, name) => {
+        const { value } = e.target;
+        setProduct((prevProduct) => ({
+            ...prevProduct,
             [name]: value,
-        });
-        console.log(product);
+        }));
     };
-    // 3. 폼 제출 핸들러
+
+    useEffect(() => {
+        // 초기값 설정 코드
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() + selectedDays);
+        const formattedDate = currentDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        });
+
+        setProduct((prevProduct) => ({
+            ...prevProduct,
+            auctionEnd: `${selectedDays}일 (${formattedDate} 마감)`,
+        }));
+    }, [selectedDays]);
+
+    const handleSetAuctionEnd = (days) => {
+        setSelectedDays(days); // 선택된 날짜 업데이트
+    };
+
+    const renderAuctionEnd = () => {
+        if (selectedDays !== null) {
+            return (
+                <div className="selected-auction-end">
+                    {product.auctionEnd}
+                </div>
+            );
+        }
+        return null;
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        var newAuctionId
-        // 폼 데이터를 서버로 던지기
+        var newAuctionId;
+
         try {
-            const response = await axiosConnect.post('/auctions',product,{
+            const response = await axiosConnect.post('/auctions', product, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-            newAuctionId=response.data.id
+            newAuctionId = response.data.id;
             console.log(response.data.id);
-        }catch(error){
+        } catch (error) {
             console.error('책 등록 전송 실패:', error);
         }
+
         window.alert("경매 등록이 완료되었습니다.");
         navigation(`/trading?id=${newAuctionId}`);
-        // 제출 후에 폼 지우기
+
         setProduct({
             auctionTitle: '',
             auctionContext: '',
             auctionPrice: '',
-            uId:'',
+            uId: '',
+            auctionEnd: '', // 초기화
         });
+        setSelectedDays(null); // 선택된 날짜 초기화
     };
-
 
     return (
         <>
-            <div className="container-fluid mt-5">
-                <form onSubmit={handleSubmit}>
+            <div className="book-sign-up-container">
+                <form onSubmit={handleSubmit} className="book-sign-up-form">
                     <div className="row justify-content-around">
                         <div className="col-md-6">
-                            <h2 className="mb-3">도서 역경매 등록</h2>
-                            <div className="form-group">
-                                <label htmlFor="name">게시글 제목:</label>
+                            <h2 className="mb-3 textH2">도서 역경매 등록</h2>
+                            <div className="form-groupName">
+                                <label htmlFor="name" style={{
+                                    marginLeft: '-1rem', marginTop: '2rem'
+                                }}>게시글 제목</label>
                                 <input
                                     type="text"
                                     className="form-control"
                                     id="auctionTitle"
                                     name="auctionTitle"
-                                    onChange={handleChange}
+                                    onChange={(e) => handleChange(e, "auctionTitle")}
+                                    placeholder="게시글 제목을 입력하세요"
                                 />
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="description">상품정보:</label>
+                            <div className="form-groupDes">
+                                <label htmlFor="description" style={{
+                                    marginLeft: '-1rem', marginTop: '1rem'
+                                }}>상세정보</label>
                                 <textarea
                                     className="form-control"
                                     id="auctionContext"
+                                    style={{ marginLeft: '-1rem' }}
                                     name="auctionContext"
-                                    onChange={handleChange}
+                                    onChange={(e) => handleChange(e, "auctionContext")}
+                                    placeholder="상세정보를 입력하세요"
                                 ></textarea>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="price">경매시작가:</label>
+                                <label htmlFor="price" style={{
+                                    marginLeft: '-1rem', marginTop: '1rem'
+                                }}>경매시작가</label>
                                 <input
                                     type="number"
                                     className="form-control"
                                     id="auctionPrice"
                                     name="auctionPrice"
-                                    onChange={handleChange}
+                                    onChange={(e) => handleChange(e, "auctionPrice")}
+                                    placeholder="원하는 경매시작 가격을 입력하세요"
                                 />
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="category">마감일:</label>
-                                <input
-                                    className="form-control"
-                                    id="auctionEnd"
-                                    name="auctionEnd"
-                                    placeholder="(D+7일로 하드코딩) 지금은 이 칸을 사용하지 않음"
-                                >
-                                </input>
+                            <div className="form-group exDateBtn-group">
+                                <label htmlFor="category" style={{
+                                    marginTop: '1rem'
+                                }}>마감기한</label>
+                                <div>{renderAuctionEnd()}</div> {/* 선택된 마감일 표시 */}
+                                <div className="btn-group exDateBtn">
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        onClick={() => handleSetAuctionEnd(1)}
+                                    >
+                                        1일
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        onClick={() => handleSetAuctionEnd(3)}
+                                    >
+                                        3일
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        onClick={() => handleSetAuctionEnd(7)}
+                                    >
+                                        7일
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        onClick={() => handleSetAuctionEnd(30)}
+                                    >
+                                        30일
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        onClick={() => handleSetAuctionEnd(60)}
+                                    >
+                                        60일
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <hr className="mt-5" />
-                    <BookResearch aucToInfo={infoCallback}/>
+                    <BookResearch aucToInfo={infoCallback} className="book-research" />
 
                     <div className="d-grid gap-2 col-11 mx-auto mt-5">
-                        <button type="submit" className="btn btn-primary btn-lg mt-5">
+                        <button type="submit" className="btn btn-sign-up btn-lg mt-5">
                             역경매 등록
                         </button>
                     </div>
@@ -137,6 +209,6 @@ const BookSignUp = () => {
             </div>
         </>
     );
-};
+}
 
 export default BookSignUp;
