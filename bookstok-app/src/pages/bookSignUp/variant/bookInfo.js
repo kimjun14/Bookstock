09484bfdc8a96a608bc00e8aa6ff725e43244ed0
SANGMIN <kwk680037@gmail.com>
2023-09-23 14:38:25 +0900

    bookInfo / UI 수정 / 유상민

--- bookstok-app/src/pages/bookSignUp/variant/bookInfo.js
+++ bookstok-app/src/pages/bookSignUp/variant/bookInfo.js
@@ -46,27 +46,28 @@ const BookResearch = ({ aucToInfo }) => {
         <div className="container-fluid mt-5">
             <div className="row align-items-center">
                 <form className="validation-form" noValidate>
-                    <div className="row justify-content-around">
+                    <div className="row justify-content-around mt-5">
                         <div className="col-md-3">
                             <img src={bookInfo.title_url} alt="bookimg" name="bookImgSrc" className="col-md-12"/>
                         </div>
-                        <div className="col-md-6 mb-3">
-                            <div className="row justify-content-between mb-3">
-                                <h2 className="mb-3 col-md-6">도서 정보</h2>
-                                <button type="button" className="btn btn-success col-md-2" onClick={openModal}>도서검색</button>
-                                <PopUp isOpen={showModal} onClose={closeModal} infoToPopup={popupCallback} />
-                            </div>
+                        <div className="col-md-5 mb-3">
+                                <div className="row justify-content-between align-items-center mb-3">
+                                    <h1 className="col-md-5">도서 정보</h1>
+                                    <button type="button" className="btn btn-success col-md-3 mb-2" onClick={openModal}>도서검색</button>
+                                    <PopUp isOpen={showModal} onClose={closeModal} infoToPopup={popupCallback} />
+                                </div>
+                            
                             <label htmlFor="email">도서제목:</label>
-                            <input type="text" className="form-control" id="bookTitle" name="bookTitle" value={bookInfo.title} />
+                            <input type="text" className="form-control ms-1" id="bookTitle" name="bookTitle" value={bookInfo.title} />
 
                             <label htmlFor="userAccount">출판사:</label>
-                            <input type="text" className="form-control" id="bookPub" name="bookPub" value={bookInfo.pub} />
+                            <input type="text" className="form-control ms-1" id="bookPub" name="bookPub" value={bookInfo.pub} />
 
                             <label htmlFor="name">출판일:</label>
-                            <input type="text" className="form-control" id="bookPubDate" name="bookPubDate" value={bookInfo.pubDate} />
+                            <input type="text" className="form-control ms-1" id="bookPubDate" name="bookPubDate" value={bookInfo.pubDate} />
 
                             <label htmlFor="address">작가:</label>
-                            <input type="text" className="form-control" id="bookAuthor" name="bookAuthor" value={bookInfo.author} />
+                            <input type="text" className="form-control ms-1" id="bookAuthor" name="bookAuthor" value={bookInfo.author} />
                         </div>
                     </div>
                 </form>
