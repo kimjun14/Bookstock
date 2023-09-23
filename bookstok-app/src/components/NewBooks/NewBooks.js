ee53c6c85db235584672721af623ca7998b7cc6f
SANGMIN <kwk680037@gmail.com>
2023-09-23 14:09:54 +0900

    MainPage NewBooks / 카드 높이 정렬 / 유상민

--- bookstok-app/src/components/NewBooks/NewBooks.js
+++ bookstok-app/src/components/NewBooks/NewBooks.js
@@ -9,30 +9,30 @@ const axiosConnect = axios.create({
     withCredentials: true
 });
 
-        // title: '어린왕자',
-        // author: '생텍쥐페리',
-        // startPrice: 11000,
-        // presentPrice: 9000,
-        // coverImage: 'https://picsum.photos/80/115',
-        // 쓰지 않으니 레거시 코드의 일부만 남깁니다.
+// title: '어린왕자',
+// author: '생텍쥐페리',
+// startPrice: 11000,
+// presentPrice: 9000,
+// coverImage: 'https://picsum.photos/80/115',
+// 쓰지 않으니 레거시 코드의 일부만 남깁니다.
 
 function NewBooks() {
-    const [bookData , setBookData] = useState([]);
+    const [bookData, setBookData] = useState([]);
 
     const newBookFetcher = async () => {
-        try{
-            const response=await axiosConnect.get('test/mainpagetest')
+        try {
+            const response = await axiosConnect.get('test/mainpagetest')
             setBookData(response.data);
-        }catch(err){
+        } catch (err) {
             console.log(err);
-        }finally{
+        } finally {
             console.log(bookData);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         newBookFetcher();
-    },[]);
+    }, []);
 
     return (
         <div>
@@ -41,9 +41,10 @@ function NewBooks() {
                 {bookData.map((bookData) => (
                     <div className="col" key={bookData.index} style={{ position: "relative" }}>
                         <Link to={`/trading?id=${bookData.auctionId}`} className="card-link">
-                            <div className="card custom-card-new">
-                                <img src={bookData.bookImgSrc} className="card-img" alt={bookData.bookTitle} />
-                                <div className="card-body">
+                            <div className="card custom-card-new" style={{ display: 'flex', flexDirection: 'column' }}>
+                                <img src={bookData.bookImgSrc} className="card-img" alt={bookData.bookTitle} style={{ height: '485px' }} />
+                                {/* Adjust the height value (e.g., '200px') to your desired fixed height */}
+                                <div className="card-body" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
                                     <h5 className="card-title">{bookData.bookTitle}</h5>
                                     <p className="card-text">{bookData.bookAuthor}</p>
                                     <p className="card-text">시작 가격: {bookData.auctionPrice}원</p>
@@ -55,6 +56,7 @@ function NewBooks() {
                 ))}
             </div>
         </div>
+
     );
 }
 
