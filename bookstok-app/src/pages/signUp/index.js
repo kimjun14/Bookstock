132dfdb37071f3dc05d8d2ac39d77defbac3ff45
최한슬 <hanseulo@naver.com>
2023-09-25 15:32:23 +0900

    회원가입 관심카테고리선택/최한슬

--- bookstok-app/src/pages/signUp/index.js
+++ bookstok-app/src/pages/signUp/index.js
@@ -10,11 +10,183 @@ import Address from "./address/Adress";
 const axiosConnect = axios.create({
     baseURL: 'http://localhost:12345/api',
     withCredentials: true
-  });
+});
+
+const categories = [
+    {
+        name: '소설',
+        subcategories: [
+            '소설 전체',
+            '한국소설',
+            '영미소설',
+            '일본 소설',
+            '중국 소설',
+            '북유럽 소설',
+            '독일 소설',
+            '프랑스 소설',
+            '기타 국가 소설',
+            '추리/미스터리/스릴러',
+            'SF 소설',
+            '국내 판타지 소설',
+            '해외 판타지 소설',
+            '국내 역사소설',
+            '해외 역사소설',
+            '대체 역사소설',
+            '동양 고전문학',
+            '서양 고전문학'
+        ],
+    },
+    {
+        name: '경영/경제',
+        subcategories: [
+            '경영/경제 전체',
+            '경영일반',
+            '경제일반',
+            '마케팅/세일즈',
+            '재테크/금융/부동산',
+            'CEO/리더십'
+        ],
+    },
+    {
+        name: '인문/사회/역사',
+        subcategories: [
+            '인문/사회/역사 전체',
+            '인문',
+            '정치/사회',
+            '예술/문화',
+            '역사'
+        ],
+    },
+    {
+        name: "자기계발",
+        subcategories: [
+            '자기계발 전체',
+            '성공/삶의자세',
+            '기획/창의/리더십',
+            '설득/화술/협상',
+            '취업/창업',
+            '여성',
+            '인간관계'
+        ]
+    },
+    {
+        name: "에세이/시",
+        subcategories: [
+            '에세이/시 전체',
+            '에세이',
+            '시'
+        ]
+    },
+    {
+        name: "여행",
+        subcategories: [
+            '여행 전체',
+            '국내여행',
+            '해외여행'
+        ]
+    },
+    {
+        name: "종교",
+        subcategories: [
+            '종교 전체',
+            '종교일반',
+            '가톨릭',
+            '기독교(개신교)',
+            '불교',
+            '기타'
+        ]
+    },
+    {
+        name: '외국어',
+        subcategories: ['외국어 전체',
+            '비즈니스영어',
+            '일반영어',
+            '제2외국어',
+            '어학시험']
+    },
+    {
+        name: '과학',
+        subcategories: [
+            '과학 전체',
+            '과학일반',
+            '수학',
+            '자연과학',
+            '응용과학']
+    },
+    {
+        name: '진로/교육/교재',
+        subcategories: ['진로/교육/교재 전체',
+            '공부법',
+            '특목고/자사고',
+            '대입 수시',
+            '대입 논술',
+            '대입 합격수기',
+            '진로 탐색',
+            '유학/MBA',
+            '교재/수험서']
+    },
+    {
+        name: '컴퓨터/IT',
+        subcategories: [
+            '컴퓨터/IT 전체',
+            'IT 비즈니스',
+            '개발/프로그래밍',
+            '컴퓨터/앱 활용',
+            'IT자격증',
+            'IT 해외원서'
+        ],
+    },
+    {
+        name: '건강/다이어트',
+        subcategories: [
+            '건강/다이어트 전체',
+            '다이어트/운동/스포츠',
+            '스타일/뷰티',
+            '건강'
+        ],
+    },
+    {
+        name: '가정/생활',
+        subcategories: [
+            '가정/생활 전체',
+            '결혼/임신/출산',
+            '육아/자녀교육',
+            '취미/요리/기타'
+        ],
+    },
+    {
+        name: "어린이/청소년",
+        subcategories: [
+            '어린이/청소년 전체',
+            '유아',
+            '어린이',
+            '청소년',
+        ]
+    },
+    {
+        name: '해외도서',
+        subcategories: ['해외도서 전체']
+    },
+    {
+        name: '잡지',
+        subcategories: ['잡지 전체',
+            '경영/재테크',
+            '문학/교양',
+            '여성/패션/뷰티',
+            '디자인/예술',
+            '건강/스포츠',
+            '취미/여행/요리',
+            '과학/IT',
+            '종교',
+            '만화']
+    }
+];
+
 
 function SignUp() {
-    const [signUpData,setSignUpData] = useState({});
-    const navigation=useNavigate();
+    const [signUpData, setSignUpData] = useState({});
+    const [selectedCategories, setSelectedCategories] = useState([]); // 선택한 카테고리 배열 추가
+    const navigation = useNavigate();
 
     const KakaoRestApiKey = '861d57b9824340a31ae9c887397ac901'; // Kakao REST API Key
     const KakaoRedirectUri = 'http://localhost:3000/oauth/callback/kakao'; // Kakao Redirect URI
@@ -38,14 +210,31 @@ function SignUp() {
         setSignUpData({
             ...signUpData,
             [e.target.name]: e.target.value
-            
+
         });
         console.log(signUpData);
     }
 
     const handleSignUpSubmit = async () => {
         try {
-            await axiosConnect.post('/users/', signUpData)
+            // 필수 입력 필드 검사
+            if (!signUpData.nick || !signUpData.userId || !signUpData.pwd || !signUpData.userPhone || !signUpData.userAccount) {
+                window.alert("모든 필수 정보를 입력하세요.");
+                return;
+            }
+
+            // 카테고리 선택 검사
+            if (selectedCategories.length < 1) {
+                window.alert("적어도 1개 이상의 카테고리를 선택하세요.");
+                return;
+            }
+
+            // 회원 가입 데이터에 선택된 카테고리 추가
+            signUpData.categories = selectedCategories;
+
+            // 서버로 데이터 전송
+            await axiosConnect.post('/users/', signUpData);
+
             window.alert("회원 등록이 완료되었습니다.");
             navigation('/signin');
         } catch (err) {
@@ -54,30 +243,54 @@ function SignUp() {
             navigation('/signup');
         }
     }
+
     //주소 처리
-        const [enroll_company, setEnroll_company] = useState({
-            address:'',
-        });
+    const [enroll_company, setEnroll_company] = useState({
+        address: '',
+    });
 
-        const [popup, setPopup] = useState(false);
+    const [popup, setPopup] = useState(false);
 
-        const handleInput = (e) => {
-            setEnroll_company({
-                ...enroll_company,
-                [e.target.name]:e.target.value,
-            })
+    const handleInput = (e) => {
+        setEnroll_company({
+            ...enroll_company,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleComplete = (data) => {
+        setPopup(!popup);
+    }
+
+    // 선택된 카테고리가 2개인지 확인하는 상태
+    const isCategoryFull = selectedCategories.length === 2;
+
+    const handleCategoryToggle = (categoryName) => {
+        // 이미 선택된 카테고리인지 확인
+        if (selectedCategories.includes(categoryName)) {
+            // 이미 선택된 경우, 해당 카테고리 제거
+            setSelectedCategories((prevSelectedCategories) =>
+                prevSelectedCategories.filter((category) => category !== categoryName)
+            );
+        } else {
+            // 최대 2개까지만 선택 가능하도록 로직 추가
+            if (selectedCategories.length < 2) {
+                // 최대 2개 미만인 경우, 카테고리 추가
+                setSelectedCategories((prevSelectedCategories) => [
+                    ...prevSelectedCategories,
+                    categoryName,
+                ]);
+            }
         }
+    };
 
-        const handleComplete = (data) => {
-            setPopup(!popup);
-        }
     return (
         <div className="container-fluid px-2 px-md-4 py-5 mx-auto">
             <div className="col-lg-7" style={{ margin: 'auto' }}>
                 <div className="card2 card border-0 px-4 px-sm-5 py-5">
                     <small className="text-right mb-3">
                         <Link to="./../signIn/">
-                            <u style={{color:'gray', textDecoration:'none'}}>북스탁 계정이 이미 있나요?</u>
+                            <u style={{ color: 'gray', textDecoration: 'none' }}>북스탁 계정이 이미 있나요?</u>
                         </Link>
                     </small>
                     <h3 className="mb-1" >회원가입</h3>
@@ -85,32 +298,32 @@ function SignUp() {
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">닉네임</h6>
                         </label>
-                        <input type="text" name="nick" placeholder="RaLiBooks" style={{ width: '50%' }} 
-                        value={signUpData.nick} onChange={handleSignUpDataChange} />
+                        <input type="text" name="nick" placeholder="RaLiBooks" style={{ width: '50%' }}
+                            value={signUpData.nick} onChange={handleSignUpDataChange} />
                     </div>
                     <div className="row px-3">
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">이메일 주소(아이디)</h6>
                         </label>
                         <input type="text" name="userId" placeholder="BookStock@email.com"
-                        value={signUpData.userId} onChange={handleSignUpDataChange} />
+                            value={signUpData.userId} onChange={handleSignUpDataChange} />
                     </div>
                     <div className="row px-3">
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">비밀번호</h6>
                         </label>
                         <input type="password" name="pwd" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                        value={signUpData.pwd} onChange={handleSignUpDataChange} />
+                            value={signUpData.pwd} onChange={handleSignUpDataChange} />
                     </div>
 
-                    
+
                     <div className="row px-3" >
-                         <label className="mb-0">
+                        <label className="mb-0">
                             <h6 className="mb-0 text-sm">주소</h6>
                         </label>
                         <button className="btn btn-primary" onClick={handleComplete}>우편번호 찾기</button>
-                        
-                        <input className="user_enroll_text" placeholder="주소"  type="text" required={true} name="address" onChange={handleInput} value={enroll_company.address}/>
+
+                        <input className="user_enroll_text" placeholder="주소" type="text" required={true} name="address" onChange={handleInput} value={enroll_company.address} />
                         {popup && <Address company={enroll_company} setcompany={setEnroll_company}></Address>}
                     </div>
 
@@ -118,16 +331,37 @@ function SignUp() {
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">전화번호</h6>
                         </label>
-                        <input type="text" name="userPhone" placeholder="-(하이픈)을 제외하고 입력하세요" 
-                        value={signUpData.userPhone} onChange={handleSignUpDataChange} />
-                        
+                        <input type="text" name="userPhone" placeholder="-(하이픈)을 제외하고 입력하세요"
+                            value={signUpData.userPhone} onChange={handleSignUpDataChange} />
+
                     </div>
                     <div className="row px-3">
                         <label className="mb-0">
                             <h6 className="mb-0 text-sm">계좌번호</h6>
                         </label>
-                        <input type="text" name="userAccount" placeholder="-(하이픈)을 제외하고 입력하세요" 
-                        value={signUpData.userAccount} onChange={handleSignUpDataChange} />
+                        <input type="text" name="userAccount" placeholder="-(하이픈)을 제외하고 입력하세요"
+                            value={signUpData.userAccount} onChange={handleSignUpDataChange} />
+                    </div>
+                    <div className="row px-3">
+                        <label className="mb-0">
+                            <h6 className="mb-0 text-sm">관심 카테고리 (최대 2개 선택)</h6>
+                        </label>
+                        <div className="category-toggle">
+                            {categories.map((category) => (
+                                <div key={category.name} className="form-check form-check-inline">
+                                    <input
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        name="categories"
+                                        value={category.name}
+                                        checked={selectedCategories.includes(category.name)}
+                                        onChange={() => handleCategoryToggle(category.name)}
+                                        disabled={isCategoryFull && !selectedCategories.includes(category.name)}
+                                    />
+                                    <label className="form-check-label">{category.name}</label>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                     <div className="row px-3 mb-3">
                         <small className="text-muted">
