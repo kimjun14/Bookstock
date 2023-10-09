ed4923320eb9061b8c9b1ba00946cc0080da9d57
최한슬 <hanseulo@naver.com>
2023-10-09 16:28:08 +0900

    searchResult/도서검색페이지 반응형수정/최한슬

--- bookstok-app/src/pages/searchResult/newIndex.js
+++ bookstok-app/src/pages/searchResult/newIndex.js
@@ -5,16 +5,6 @@ import axios from 'axios';
 import moment from 'moment';
 import { useMediaQuery } from 'react-responsive';
 
-const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 })
-  return isDesktop ? children : null
-}
-
-const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 576 })
-  return isMobile ? children : null
-}
-
 // axios 통신에 기본 url을 포함시키고 Credentials 옵션을 붙여서 쿠키전송 가능하게 함
 const axiosConnect = axios.create({
   baseURL: 'http://localhost:12345/api',
@@ -22,6 +12,8 @@ const axiosConnect = axios.create({
 });
 
 function BookSearchResult() {
+  const isTablet = useMediaQuery({ minWidth: 576, maxWidth: 991 });
+  const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 575 });
   // eslint-disable-next-line
   const navigation = useNavigate();
   const URLquery = useLocation();
@@ -31,22 +23,22 @@ function BookSearchResult() {
   const [books, setBooks] = useState([]);
 
   const fetchSearchList = async () => {
-    try{
-        const response=await axiosConnect.get(`/auctions/search?query=${searchKey}`)
-        setBooks(response.data);
-    }catch(err){
-        console.error(err);
+    try {
+      const response = await axiosConnect.get(`/auctions/search?query=${searchKey}`)
+      setBooks(response.data);
+    } catch (err) {
+      console.error(err);
     }
   }
 
-  useEffect(()=>{
-    if(URLquery.search){
+  useEffect(() => {
+    if (URLquery.search) {
       fetchSearchList(searchKey);
-    }else{    // URL/searchResult 같이 쿼리 없이 접근하면
+    } else {    // URL/searchResult 같이 쿼리 없이 접근하면
       alert("잘못 된 접근입니다.");    // id쿼리 없이 들어가면 오류 메세지 나오고
       navigation('/');                // 홈('/')화면으로 보내버림 (추후 변경 할 수도)
     }
-  },[searchKey]);
+  }, [searchKey]);
 
   const AuctionCreateAt = (dateString) => {
     const Date = moment(dateString).format("YYYY-MM-DD HH:mm:ss");
@@ -54,8 +46,6 @@ function BookSearchResult() {
   }
 
   return (
-    <>
-    <Desktop>
     <div>
       <h1 style={{ textAlign: 'center' }}>도서 검색 결과</h1>
       {/* 정렬 버튼 */}
@@ -65,7 +55,7 @@ function BookSearchResult() {
         <button type="button" className="btn btn-pop">인기순</button>
       </div>
       {/* 검색 결과 표 */}
-      <table className="searchTable">
+      <table className={`searchTable ${isMobile ? 'mobile' : isTablet ? 'tablet' : ''}`}>
         <thead style={{
           backgroundColor: '#f3f3f3'
         }}>
@@ -80,14 +70,14 @@ function BookSearchResult() {
             <th scope="col">관심등록</th>
           </tr>
         </thead>
-        <tbody className="table-group-divider"  style={{marginBottom:'2rem'}}>
+        <tbody className="table-group-divider" style={{ marginBottom: '2rem' }}>
           {books.map((book) => (
             <tr key={book.index}>
               <th scope="row">{book.index}</th>
-              <td><img src={book.bookImgSrc ? book.bookImgSrc : "http://via.placeholder.com/120x160"} alt="" className='searchBookImg'/></td>
+              <td><img src={book.bookImgSrc ? book.bookImgSrc : "http://via.placeholder.com/120x160"} alt="" className='searchBookImg' /></td>
               <td><Link to={`/trading?id=${book.auctionId}`}>{book.bookTitle}</Link></td>
               <td>시작가: {book.auctionPrice}원
-              <br />현재가: 추후 구현 예정</td>
+                <br />현재가: 추후 구현 예정</td>
               <td><Link to={`/`}>{book.nickname} </Link></td>
               <td>{AuctionCreateAt(book.auctionStart)} /<br />{AuctionCreateAt(book.auctionEnd)}</td>
               <td>{book.viewCount}</td>
@@ -108,62 +98,6 @@ function BookSearchResult() {
         </tbody>
       </table>
     </div>
-    </Desktop>
-    <Mobile>
-    <div>
-      <h1 style={{ textAlign: 'center' }}>도서 검색 결과</h1>
-      {/* 정렬 버튼 */}
-      <div className="btn-group" role="group" aria-label="Basic example">
-        <button type="button" className="btn btn-recent">최신순</button>
-        <button type="button" className="btn btn-title">제목순</button>
-        <button type="button" className="btn btn-pop">인기순</button>
-      </div>
-      {/* 검색 결과 표 */}
-      <table className="searchTable">
-        <thead style={{
-          backgroundColor: '#f3f3f3'
-        }}>
-          <tr>
-            <th scope="col">번호</th>
-            <th scope="col">표지</th>
-            <th scope="col">도서명</th>
-            <th scope="col">가격</th>
-            <th scope="col">등록자명</th>
-            <th scope="col">등록일/마감일</th>
-            <th scope="col">조회수</th>
-            <th scope="col">관심등록</th>
-          </tr>
-        </thead>
-        <tbody className="table-group-divider"  style={{marginBottom:'2rem'}}>
-          {books.map((book) => (
-            <tr key={book.index}>
-              <th scope="row">{book.index}</th>
-              <td><img src={book.bookImgSrc ? book.bookImgSrc : "http://via.placeholder.com/120x160"} alt="" className='searchBookImg'/></td>
-              <td><Link to={`/trading?id=${book.auctionId}`}>{book.bookTitle}</Link></td>
-              <td>시작가: {book.auctionPrice}원
-              <br />현재가: 추후 구현 예정</td>
-              <td><Link to={`/`}>{book.nickname} </Link></td>
-              <td>{AuctionCreateAt(book.auctionStart)} /<br />{AuctionCreateAt(book.auctionEnd)}</td>
-              <td>{book.viewCount}</td>
-              <td>
-
-                {/* 관심등록 버튼 */}
-                <button className='interestBtn'>
-                  <img
-                    src="http://localhost:12345/images/star.png"
-                    alt=""
-                    width="20px"
-                  />
-                </button>
-                0
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </Mobile>
-    </>
   );
 }
 
